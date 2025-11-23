486b8169b93f708ac42592d2b3467316b1b2fa01
Abhinav <abhinavdasu0412@gmail.com>
2025-11-23 18:30:06 +0530

    test deploy

--- website-real/next.config.ts
+++ website-real/next.config.ts
@@ -3,9 +3,9 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {
-    root: path.resolve(__dirname),
-  },
+  // turbopack: {
+  //   root: path.resolve(__dirname),
+  // },
 };
 
 export default nextConfig;
