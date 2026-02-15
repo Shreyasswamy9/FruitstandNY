98ce0f7c6f7aee6d17c6e944ba247ecefbbe6e6d
Shreyas Swamy <ss263@fordham.edu>
2026-02-15 14:45:24 -0500

    conneciton

--- website-real/app/layout.tsx
+++ website-real/app/layout.tsx
@@ -50,13 +50,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <MetaPixelBase />
+        <script async src="https://chimpstatic.com/mcjs-connected/js/users/fcf0bbb9c363876e4b7950198/177293cf6369623cf7ebb592e.js"></script>
         <Script id="google-tag-manager" strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-N8M6F5WK');`}
         </Script>
-        <Script id="mcjs" strategy="afterInteractive">{`!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/fcf0bbb9c363876e4b7950198/177293cf6369623cf7ebb592e.js");`}</Script>
       </head>
       <body 
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#fbf6f0]`}
