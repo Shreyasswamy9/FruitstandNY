bd9878f5437aff4d4a0aee0ef14cc40384ba915e
Shreyas Swamy <ss263@fordham.edu>
2026-03-25 13:34:03 -0400

    mailchimp update

--- website-real/app/layout.tsx
+++ website-real/app/layout.tsx
@@ -119,7 +119,7 @@ export default function RootLayout({
         <script
           id="mcjs"
           dangerouslySetInnerHTML={{
-            __html: '!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/fcf0bbb9c363876e4b7950198/177293cf6369623cf7ebb592e.js");'
+            __html: '!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/fcf0bbb9c363876e4b7950198/701c39750148b2925ed7905a1.js");'
           }}
         />
         <Script id="google-tag-manager" strategy="afterInteractive">{`
